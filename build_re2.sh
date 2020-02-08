8c4ce1790079a9d039172650150666b0fa86b7dc
lockone <dkcjd2000@gmail.com>
2020-02-08 22:14:40 +0900

    fix

--- build_re2.sh
+++ build_re2.sh
@@ -1,8 +1,8 @@
 make clean
 rm fuzz fuzz.bc fuzz.fast fuzz.o fuzz.tt
-LDFLAGS="-L/local_home/cheong/Angora_funb/bin/lib/libcxx_fast -lc++" CXX=gclang++ CXXFLAGS="-v -stdlib=libc++" make
+LDFLAGS="-lc++" CXX=gclang++ CXXFLAGS="-v -stdlib=libc++" make
 
 get-bc fuzz
 
-LDFLAGS="-L/local_home/cheong/Angora_funb/bin/lib/libcxx_fast -lc++" CXXFLAGS="-v -stdlib=libc++" /local_home/cheong/Angora_func/bin/angora-clang++ fuzz.bc -o fuzz.fast
-LDFLAGS="-L/local_home/cheong/Angora_funb/bin/lib/libcxx_fast -lc++" CXXFLAGS="-v -stdlib=libc++" USE_TRACK=1  /local_home/cheong/Angora_func/bin/angora-clang++ fuzz.bc -o fuzz.tt
+LDFLAGS="-lc++" CXXFLAGS="-v -stdlib=libc++" /local_home/cheong/Angora_func/bin/angora-clang++ fuzz.bc -o fuzz.fast
+LDFLAGS="-lc++" CXXFLAGS="-v -stdlib=libc++" USE_TRACK=1  /local_home/cheong/Angora_func/bin/angora-clang++ fuzz.bc -o fuzz.tt
