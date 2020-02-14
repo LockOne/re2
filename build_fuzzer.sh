ad6ea92369575b08744924b28aa326dfbd7caa97
cheong <cheong@tester.kaist.ac.kr>
2020-02-15 01:39:37 +0900

    config

--- build_fuzzer.sh
+++ build_fuzzer.sh
@@ -2,12 +2,12 @@ ANGORA_LOC=/local_home/cheong/Angora_func
 
 make clean
 rm fuzz fuzz.bc fuzz.fast fuzz.o fuzz.tt
-LDFLAGS="-lc++" CXX=gclang++ CXXFLAGS="-v -stdlib=libc++ -I/usr/include/c++/8 -I/usr/include/c++/8" make
+LDFLAGS="-lc++" CXX=gclang++ CXXFLAGS="-v -stdlib=libc++ -I/usr/include/c++/8 -I/usr/include/x86_64-linux-gnu/c++/8" make
 
 get-bc fuzz
 
 #LDFLAGS="-lc++"
-CXXFLAGS="-v -stdlib=libc++ -I/usr/include/c++/8 -I/usr/include/c++/8" ${ANGORA_LOC}/bin/angora-clang++ fuzz.bc -o ${ANGORA_LOC}/subjects/re2.fast
+CXXFLAGS="-v -stdlib=libc++ -I/usr/include/c++/8 -I/usr/include/x86_64-linux-gnu/c++/8" ${ANGORA_LOC}/bin/angora-clang++ fuzz.bc -o ${ANGORA_LOC}/subjects/re2.fast
 mv FuncInfo.txt ${ANGORA_LOC}/FInfos/FuncInfo-re2.txt
 #LDFLAGS="-lc++"
-CXXFLAGS="-v -stdlib=libc++ -I/usr/include/c++/8 -I/usr/include/c++/8" USE_TRACK=1  ${ANGORA_LOC}/bin/angora-clang++ fuzz.bc -o ${ANGORA_LOC}/subjects/re2.tt
+CXXFLAGS="-v -stdlib=libc++ -I/usr/include/c++/8 -I/usr/include/x86_64-linux-gnu/c++/8" USE_TRACK=1  ${ANGORA_LOC}/bin/angora-clang++ fuzz.bc -o ${ANGORA_LOC}/subjects/re2.tt
