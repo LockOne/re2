9218c47b8b3de4b3a6acd1e13d0472e4d623a4b6
Cheong Lee <cheong@debian.kaist.ac.kr>
2020-02-17 13:24:25 +0900

    fix

--- build_fuzzer.sh
+++ build_fuzzer.sh
@@ -2,12 +2,10 @@ ANGORA_LOC=/local_home/cheong/Angora_func
 
 make clean
 rm fuzz fuzz.bc fuzz.fast fuzz.o fuzz.tt
-LDFLAGS="-lc++" CXX=gclang++ CXXFLAGS="-v -stdlib=libc++ -I/usr/include/c++/8 -I/usr/include/x86_64-linux-gnu/c++/8" make
+CXX=gclang++  make
 
 get-bc fuzz
 
-#LDFLAGS="-lc++"
-CXXFLAGS="-v -stdlib=libc++ -I/usr/include/c++/8 -I/usr/include/x86_64-linux-gnu/c++/8" ${ANGORA_LOC}/bin/angora-clang++ fuzz.bc -o ${ANGORA_LOC}/subjects/re2.fast
+${ANGORA_LOC}/bin/angora-clang++ fuzz.bc -o ${ANGORA_LOC}/subjects/re2.fast
 mv FuncInfo.txt ${ANGORA_LOC}/FInfos/FuncInfo-re2.txt
-#LDFLAGS="-lc++"
-CXXFLAGS="-v -stdlib=libc++ -I/usr/include/c++/8 -I/usr/include/x86_64-linux-gnu/c++/8" USE_TRACK=1  ${ANGORA_LOC}/bin/angora-clang++ fuzz.bc -o ${ANGORA_LOC}/subjects/re2.tt
+USE_TRACK=1  ${ANGORA_LOC}/bin/angora-clang++ fuzz.bc -o ${ANGORA_LOC}/subjects/re2.tt
